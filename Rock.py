56e52f7cbdce222a8bb41bc1a6bbd060aac03b2b
IlianaT11 <ilene901111@gmail.com>
2024-06-14 17:50:27 +0300

    Rock

--- Rock.py
+++ Rock.py
@@ -5,3 +5,10 @@ for index in range(start_index, final_index + 1):
         print(chr(index))
     else:
         print(chr(index), end=" ")
+start_index = int(input())
+final_index = int(input())
+new_string = ""
+for index in range(start_index, final_index + 1):
+    new_string += chr(index) + " "
+print(new_string.strip())
+# strip - remove intervals from start and final
